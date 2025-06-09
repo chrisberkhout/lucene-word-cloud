53e39009e08c1651512e6e0db07167f7bd228fe7
Chris Berkhout <chrisberkhout@gmail.com>
2025-06-09 10:57:39 +0200

    Add javalin, html with wordcloud example.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,15 +2,15 @@ package org.example;
 
 import java.util.List;
 
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
 public class App {
     public static void main(String[] args) {
         Bible bible = new Bible();
         bible.load();
         SectionIndex index = new SectionIndex();
         index.build(bible);
-//        for (TopWords.ScoredWord w : index.getScoredWords()) {
-//            System.out.println(w.score() + "  " + w.word());
-//        }
         long start = System.nanoTime();
         List<TopWords.ScoredWord> tw = index.query();
         long end = System.nanoTime();
@@ -19,5 +19,11 @@ public class App {
         }
         System.out.println("");
         System.out.println("query time: " + ((end-start) / 1_000_000) + " ms");
+
+        var app = Javalin.create(config -> {
+                config.staticFiles.add("/public");
+            })
+            .get("/hello", ctx -> ctx.result("Hello World"))
+            .start(7070);
     }
 }
